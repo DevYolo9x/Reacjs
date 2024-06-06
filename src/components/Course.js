479386e6e15e6a40a27eadda5f714dc99f916023
binhtx <xuanbinhtxb@gmail.com>
2024-06-06 22:58:58 +0700

    Ket thuc phan State

--- src/components/Course.js
+++ src/components/Course.js
@@ -6,7 +6,13 @@ class Course extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      isShow : true
+    }
+
     this.handleClickThis = this.handleClickThis.bind(this);
+    this.handleRegis = this.handleRegis.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
   }
 
   handleClick() {
@@ -18,7 +24,17 @@ class Course extends Component {
   }
   
   handleClickThis() {
-    alert(this.props.name_button);
+    alert(this.props.name);
+  }
+
+  handleRegis(){
+    console.log(this.refs.username.value);
+  }
+
+  handleToggle(){
+    this.setState({
+      isShow: !this.state.isShow
+    });
   }
 
   showButton() {
@@ -26,15 +42,35 @@ class Course extends Component {
     if (checkType==true) {
       return (
         <div>
+          <div>
             <button type="button" onClick={this.handleClick} className="btn btn-lg btn-block btn-outline-primary mt-3">Register</button>
             <button type="button" onClick={() => this.handleClickText('Login')} className="btn btn-lg btn-block btn-outline-primary mt-3">Login</button>
-            <button type="button" onClick={this.handleClickThis} name_button="Purchase" className="btn btn-lg btn-block btn-outline-primary mt-3">Purchase</button>
+            <button type="button" onClick={this.handleClickThis} className="btn btn-lg btn-block btn-outline-primary mt-3">Purchase</button>
+          </div>
+
+          <div className="input-group mb-3 mt-3">
+            <div className="input-group-prepend">
+              <button className="btn btn-primary" onClick={this.handleRegis}>Search</button>
+            </div>
+            <input type="text" className="form-control" placeholder="Keyword..." ref="username" aria-label="Username" aria-describedby="basic-addon1" />
+          </div>
+
+          <div>
+            <button type="button" onClick={this.handleToggle} className="btn btn-success mt-3">Toggle!</button>
+          </div>
+
         </div>
       )
     }
   }
 
   render() {
+    let elShow = null;
+    if( this.state.isShow ){
+      elShow = (
+        <Lesson list={this.props.list} />
+      )
+    }
     return (
       <div className="col-lg-4 card mb-4 shadow-sm">
         <div className="card-header mt-3">
@@ -42,7 +78,7 @@ class Course extends Component {
         </div>
         <div className="card-body">
           <h1 className="card-title pricing-card-title">${this.props.price} <small className="text-muted">/ month</small></h1>
-          <Lesson list={this.props.list} />
+          {elShow}
           {this.showButton()}
         </div>
       </div>
